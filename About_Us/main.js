a6722b949cb1200406ddd6565a23233749601e8f
DillonDONE <dillondonetta7@gmail.com>
2024-05-06 18:04:29 +0700

    update

--- About_Us/main.js
+++ About_Us/main.js
@@ -1,8 +1,16 @@
-var swiper = new Swiper(".swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  gragCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
