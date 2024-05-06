3fe82feb8252ff85b3c018a3cd416eb82fbe2c4d
DillonDONE <dillondonetta7@gmail.com>
2024-05-06 22:20:48 +0700

    update

--- About_Us/main.js
+++ About_Us/main.js
@@ -14,3 +14,26 @@ var swiper = new Swiper(".slide-content", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slid');
+
+let currentIndex = 0;
+let slideWidth = slides[currentIndex].clientWidth;
+
+function updateSlideWidth() {
+  slideWidth = slides[currentIndex].clientWidth;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSlideWidth(); 
+  updateSlider();
+}
+
+function updateSlider() {
+  const offset = -currentIndex * slideWidth;
+  slider.style.transform = `translateX(${offset}px)`;
+}
+
+setInterval(nextSlide, 3000); 
